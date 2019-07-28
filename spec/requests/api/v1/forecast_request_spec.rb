3eeacf1d83cd8af31ac9040c1edf4d151b27511e
lpile <logan.pile@gmail.com>
2019-07-28 14:46:54 -0600

    Update json weather information for a location spec

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
-describe "Weather API" do
-  it "sends lat and long for location" do
+describe "Forecast API", type: :request do
+  it "returns json weather information for a location" do
 
     get '/api/v1/forecast?location=denver,co'
 
     expect(response).to be_successful
+
+    result = JSON.parse(response.body)
+
+    expect(result).to have_key('data')
+    expect(result['data']).to have_key('location')
+    expect(result['data']).to have_key('currently')
+    expect(result['data']).to have_key('hourly')
+    expect(result['data']['hourly'].count).to eq(8)
+    expect(result['data']).to have_key('daily')
+    expect(result['data']['daily'].count).to eq(5)
   end
 end
