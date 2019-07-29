6a756983b858409dba3b6196bbb14f13628acf06
lpile <logan.pile@gmail.com>
2019-07-29 16:47:42 -0600

    Add edge cases

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -19,5 +19,18 @@ describe "Forecast API", type: :request do
     expect(result['data']['hourly'].count).to eq(8)
     expect(result['data']).to have_key('daily')
     expect(result['data']['daily'].count).to eq(5)
+
+  end
+
+  it "returns status code 406 if content type is not application/json'" do
+
+    get '/api/v1/forecast?location=denver,co'
+
+    result = JSON.parse(response.body)
+
+    expect(response).to have_http_status(406)
+    expect(result).to have_key('error')
+    expect(result['error']).to eq('Content-Type must be application/json')
+
   end
 end
