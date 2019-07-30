34cd1f0faf1a6ac05fd1150766df3e2e84e7ffe1
lpile <logan.pile@gmail.com>
2019-07-30 10:01:05 -0600

    Refactor `render json`

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      render json: UserSerializer.new(user).api_key_response, status: 200
+    if user&.authenticate(params[:password])
+      render json: { api_key: user.api_key }, status: 200
     else
-      render json: UserSerializer.new.login_error_response, status: 406
+      render json: {error: 'Failed to login.'}, status: 406
     end
   end
 end
