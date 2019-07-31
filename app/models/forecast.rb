3a057ca197c919cb9cc627aa500155748a9cc35b
lpile <logan.pile@gmail.com>
2019-07-30 23:56:32 -0600

    Fix time zone issues

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,5 +1,5 @@
 class Forecast
-  attr_reader :id, :location, :currently_data, :hourly_data, :daily_data
+  attr_reader :id, :location, :currently_data, :hourly_data, :daily_data, :offset
 
   def initialize(forecast_data, location)
     @id = 0
@@ -7,12 +7,13 @@ class Forecast
     @currently_data = forecast_data[:currently]
     @hourly_data = forecast_data[:hourly][:data].first(8)
     @daily_data = forecast_data[:daily][:data].first(6)
+    @offset = forecast_data[:offset]
   end
 
   def currently
     {
       location: location,
-      time: Time.at(currently_data[:time]).to_datetime.strftime('%I:%M%p %m/%d/%y'),
+      time: Time.at(currently_data[:time]).in_time_zone(offset).strftime('%I:%M%p %m/%d/%y'),
       summary: currently_data[:summary],
       icon: currently_data[:icon],
       temperature: currently_data[:temperature].round,
@@ -28,7 +29,7 @@ class Forecast
   def hourly
     hourly_data.map do |index|
       {
-        time: Time.at(index[:time]).to_datetime.strftime('%I:%M%p'),
+        time: Time.at(index[:time]).in_time_zone(offset).strftime('%I:%M%p'),
         icon: index[:icon],
         temperature: index[:temperature].round
       }
@@ -38,7 +39,7 @@ class Forecast
   def daily
     daily_data[1..5].map do |index|
       {
-        time: Time.at(index[:time]).to_datetime.strftime('%A'),
+        time: Time.at(index[:time]).in_time_zone(offset).strftime('%A'),
         icon: index[:icon],
         precipProbability: (index[:precipProbability] * 100).to_i,
         precipType: index[:precipType],
